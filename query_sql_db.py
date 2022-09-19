f0e5b2afd1e9bd786e14153a81536e418c8ceabe
Dingkun Yang <81750079+Yer1k@users.noreply.github.com>
2022-09-19 06:44:37 +0000

    adding databricks

---
+++ query_sql_db.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import click
+from dblib.querydb import querydb
+
+# build a click group
+@click.group()
+def cli():
+    """A simple CLI to query a SQL database"""
+
+
+# build a click command
+@cli.command()
+@click.option(
+    "--query",
+    default="SELECT * FROM default.events_all LIMIT 2",
+    help="SQL query to execute",
+)
+def cli_query(query):
+    """Execute a SQL query"""
+    querydb(query)
+
+
+# run the CLI
+if __name__ == "__main__":
+    
+    cli()
